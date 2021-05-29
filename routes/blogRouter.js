087f829fff63d993574e27bcb349a08eb5966a35
Ertugrul <teknoertugrul@gmail.com>
2021-05-29 19:43:46 +0300

    Home controller require problem solved

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Home } = require("../controllers/home");
+const Home = require("../controllers/home");
 
 router.get("/", (req, res) => new Home(req, res));
 
