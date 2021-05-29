321bcb4e3bfcc76e78c6a5e31b247cd960ad03d9
ertugrulsencer <teknoertugrul@gmail.com>
2021-05-30 00:10:08 +0300

    Public folder and asset files added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const blogRouter = require("./routes/blogRouter");
 const app = express();
@@ -5,14 +6,16 @@ const handlebars = require("express-handlebars");
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://ertugrul:ert.blog.2021@main.fiubi.mongodb.net/blog?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(process.env.MONGO_DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     app.listen(5000, () => console.log("Server working !"));
   })
   .catch((err) => console.error(err));
+
+app.use(express.static("public"));
 
 app.engine("handlebars", handlebars());
 app.set("view engine", handlebars);
