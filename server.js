333aee47a5d59a089cba2d88565b5f9b15d5ba9b
Ertugrul <teknoertugrul@gmail.com>
2021-05-29 03:38:49 +0300

    Server port changed

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ app.get("/", (req, res) => {
   console.log("It's working !");
   res.status(200).send("It's working !");
 });
-app.listen(8000, () => console.log("Server working !"));
+app.listen(5000, () => console.log("Server working !"));
