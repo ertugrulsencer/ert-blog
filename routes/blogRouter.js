a4d87e1b7075e055c63c61399e60d909e4b59673
ertugrulsencer <teknoertugrul@gmail.com>
2021-06-12 19:31:59 +0300

    Home route added

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -3,6 +3,7 @@ const Home = require("../controllers/home");
 const About = require("../controllers/about");
 
 router.get("/", (req, res) => new Home(req, res));
+router.get("/home", (req, res) => res.redirect("/"));
 router.get("/about", (req, res) => new About(req, res));
 router.get("/about-me", (req, res) => res.redirect("/about"));
 
